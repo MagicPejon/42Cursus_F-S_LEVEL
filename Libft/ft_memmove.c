3bba3b19071b9379a4f09665feb496110147ff6c
Ammar Albreiki <81860117+MagicPejon@users.noreply.github.com>
2022-01-24 16:34:07 +0400

    Add files via upload

---
+++ Libft/ft_memmove.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalbrei <amalbrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/23 19:16:51 by amalbrei          #+#    #+#             */
+/*   Updated: 2022/01/23 19:43:47 by amalbrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *memmove(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char		*cdest;
+	const unsigned char	*csrc;
+
+	cdest = (unsigned char*)dest;
+	csrc  = (unsigned char*)src;
+	i = 0;
+	if (!dest && !src)
+		return (0);
+	if (csrc < cdest)
+		while (++i <= n)
+			cdest[n - i] = csrc[n - i];
+	else
+		while (n-- > 0)
+		{
+			cdest[i] = csrc[i];
+			i++;
+		}
+	return (dest);
+}
---
+++ Libft/libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalbrei <amalbrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/21 19:45:57 by amalbrei          #+#    #+#             */
+/*   Updated: 2022/01/21 19:45:59 by amalbrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include "unistd.h"
+#include "stdlib.h"
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_strlen(char *c);
+
+#endif
